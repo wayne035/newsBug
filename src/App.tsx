3914921353098c13133941b300d4fc5b73ca8175
wayne <QWE24367705@gmail.com>
2024-02-22 00:46:43 +0800

    增加infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect,useState } from "react"
 import Button from "./components/Button"
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 interface News{
   name: string,
@@ -12,34 +13,36 @@ interface News{
 export default function page() {
   const [data, setData] = useState([] as News[])
   const [keyWord, setKeyWord] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
+  const [hasMore, setHasMore] = useState(true)
+  const [page, setPage] = useState(0)
 
   async function handleKeyWord(keyword: string){
     setKeyWord(keyword)
     window.scrollTo(0,0)
   }
 
-  useEffect(()=> {
-    async function getData(){
-      try{
-        const res = await fetch(import.meta.env.VITE_URL)
-        const data = await res.json()
-        setData(data)
-        setIsLoading(false)
-      }catch(e){
-        console.log((e as Error).message)
+  async function getData(page: string|number){
+    setTimeout(async()=> {
+      if(data.length < 520){
+        try{
+          const res = await fetch(import.meta.env.VITE_URL + `/${page}`)
+          const data = await res.json()
+          setData(pre => [...pre,...data])
+        }catch(e){
+          console.log((e as Error).message)
+        }
       }
-    }
+      else{
+        setHasMore(false)
+      }
+    }, 300);
+  }
 
-    getData()
-  },[])
+  useEffect(()=> {
+    getData(page)
+  },[page])
 
   return (
-    isLoading ? 
-      <span className="text-[#fff] md:text-[108px] font-bold absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
-        載入中...
-      </span> 
-    :
       <>
         <header className="fixed bg-[#000] border-b-2 top-0 left-0 w-full">
           <h1 className="absolute top-0 left-0 text-transparent">newsBug</h1>
@@ -48,22 +51,33 @@ export default function page() {
           </nav>
         </header>
         <span className="block h-[180px] md:h-[120px]"></span>
-        {data?.filter(event=> keyWord ? (event.name === keyWord) : true )
-          .map(event=> (
-            <article key={crypto.randomUUID()} className="w-full flex justify-center">
-              <a href={event.link} target="_blank" title={event.title} className='flex w-full md:w-[80%] m-3 p-2 md:text-[18px] text-[#fff] border-b-2 border-[#bbb] font-bold hover:bg-[#ddd] hover:text-[#000] items-center'>
-                <time className="text-[12px] leading-[25px] w-[30px] md:w-[70px]">
-                  {event.time}
-                </time>
-                <h2 className="mx-2 md:mx-5 md:text-[14px] text-center leading-[25px] px-1 h-[25px] overflow-hidden w-[70px]" style={{background: `${event.color}`}}>
-                  {event.name}
-                </h2>
-                <h3 className="w-[260px] md:w-auto">
-                  {event.title.substring(0, 30) + "..."}
-                </h3>
-              </a>
-            </article>
-          ))}
+        <InfiniteScroll 
+          dataLength={data.length} 
+          next={()=> setPage(prev=> prev += 1)}
+          hasMore={hasMore} 
+          loader={
+            <span className="text-[#eaff49] text-[108px] font-bold w-full text-center block"> 
+              載入中......
+            </span>
+          }
+        >
+          {data?.filter(event=> keyWord ? (event.name === keyWord) : true )
+            .map(event=> (
+              <article key={crypto.randomUUID()} className="w-full flex justify-center">
+                <a href={event.link} target="_blank" title={event.title} className='flex w-full md:w-[80%] m-3 p-2 md:text-[18px] text-[#fff] border-b-2 border-[#bbb] font-bold hover:bg-[#ddd] hover:text-[#000] items-center'>
+                  <time className="text-[12px] leading-[25px] w-[30px] md:w-[70px]">
+                    {event.time}
+                  </time>
+                  <h2 className="mx-2 md:mx-5 md:text-[14px] text-center leading-[25px] px-1 h-[25px] overflow-hidden w-[70px]" style={{background: `${event.color}`}}>
+                    {event.name}
+                  </h2>
+                  <h3 className="w-[260px] md:w-auto">
+                    {event.title.substring(0, 30) + "..."}
+                  </h3>
+                </a>
+              </article>
+            ))}
+          </InfiniteScroll>
         <footer className="text-[#fff] w-full text-center py-8 font-black">
           © 2024 All Rights Reserved. Designed By Wayne
         </footer>
