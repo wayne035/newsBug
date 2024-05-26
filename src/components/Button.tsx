5505aac1e95dfff73522c795c9262ecccd315fe9
wayne <QWE24367705@gmail.com>
2024-05-26 21:12:39 +0800

    刪除部分新聞

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,6 @@ interface News{
 const btnData = [
   {'name': '全部新聞', 'en': 'all', 'color':'#555555'},
   {'name': '三立', 'en': 'setn', 'color':'#065C05'},
-  {'name': '民視', 'en': 'ftv', 'color':'#065C05'},
   {'name': '自由時報', 'en': 'ltn', 'color':'#065C05'},
   {'name': 'Newtalk', 'en': 'newtalk', 'color':'#80BF73'},
   {'name': '壹蘋果', 'en': 'apple', 'color':'#80BF73'},
@@ -19,8 +18,6 @@ const btnData = [
   {'name': '風傳媒', 'en': 'storm', 'color':'#62C2FE'},
   {'name': '聯合', 'en': 'udn', 'color':'#62C2FE'},
   {'name': 'ETtoday', 'en': 'ettoday', 'color':'#62C2FE'},
-  {'name': '東森', 'en': 'ebc', 'color':'#262CC9'},
-  {'name': '中時', 'en': 'ct', 'color':'#262CC9'},
 ]
 
 interface ButtonProps {
